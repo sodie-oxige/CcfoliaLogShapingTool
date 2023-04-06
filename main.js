1b453e7e3532af67f64e3da4fcc9aaf3c73be62b
袖置 <sodie.oxige@gmail.com>
2023-04-06 15:30:47 +0900

    v1.1
    正規表現対応（細かなon/off可）
    デフォルト設定の変更
    　秘匿タブの追加
    　雑談タブの文字色を見やすく（暗めに）
    　背景色を暗く
    設定ﾘｾｯﾄボタンの追加。デフォルトにもどせるように（更新されたデフォルト設定の確認にも使ってくださいな。）

--- main.js
+++ main.js
@@ -1,47 +1,56 @@
 window.onload=function(){ if(document.title=="ccfolia - logs"){chrome.storage.local.get(null, function(result){
-	if(result.html==undefined)result={"html":[[0,"css","div,p,span{\n\tpadding:0;margin:0;\n\tposition:relative;\n\tbox-sizing:border-box;\n}\nheader{\n\tposition:fixed;\n\ttop:0; left:0;\n\theight:1rem;\n\twidth:100%;\n\tbackground:black;\n\tcolor:white;\n\tfont-size:0.8em;\n\tz-index:1;\n}\nbody{\n\tmargin:1em auto 0;\n\tmax-width:1000px;\n\tword-break:keep-all;\n\tbackground-color:#eee;\n\toverflow-x:hidden;\n}\n.main,\n.chat{\n\tpadding:0.5em 0;\n\tdisplay:flex;\n\tgap:10px;\n\tborder-bottom:thin solid black;\n\tborder-image:linear-gradient(to right,white,silver 10%,silver 90%,white);\n\tborder-image-slice:1;\n}\n.main>p,\n.chat>p{\n\tmargin:auto 0;\n\theight:100%;\n}\n.chat>p{\n\tcolor:var(--c);\n}\n.main>p:first-child,\n.chat>p:first-child{\n\tmin-width:9em;\n\tmax-width:9em;\n\tpadding-left:0.5em;\n}\n.main::before,\n.chat::before{\n\tcontent:\"\";\n\tposition:absolute;\n\tleft:9em; top:4px;\n\twidth:2px; height:calc(100% - 8px);\n\tbackground-color:var(--c);\n}\n\n.main{\n\tbackground-color:white;\n}\n.chat{\n\tmargin-left:4em;\n\tbackground-color:#f8f8f8;\n}\n.info{\n\tmargin:5px;\n\tletter-spacing:0.1em;\n\tfont-weight:bold;\n\tfont-family:system-ui;\n}\n.info>span{\n\tfont-size:.8em;\n}\n.info>p{\n\ttext-align:center;\n}"],[0,"header","<p>{filename}</p>"],[0,"footer",""],[1,"system","\t<div class=\"info\" style=\"--c:{color}\">\n\t\t<p>{text}</p>\n\t</div>"],[0,"メイン","\t<div class=\"main\" style=\"--c:{color}\">\n\t\t<p>{author}</p>\n\t\t<p>{text}</p>\n\t</div>"],[0,"雑談","\t<div class=\"chat\" style=\"--c:{color}\">\n\t\t<p>{author}</p>\n\t\t<p>{text}</p>\n\t</div>"],[0,"情報","\t<div class=\"info\" style=\"--c:{color}\">\n\t\t<span>{author}</span>\n\t\t<p>{text}</p>\n\t</div>"]]};
+	if(result.html==undefined)result={"html":{"css":"div,p,span{\n\tpadding: 0;\n\tmargin: 0;\n\tposition: relative;\n\tbox-sizing: border-box;\n}\nheader{\n\tposition: fixed;\n\ttop: 0;\n\tleft: 0;\n\theight: 1rem;\n\twidth: 100%;\n\tbackground: black;\n\tcolor: white;\n\tfont-size: .8em;\n\tz-index: 1;\n}\nbody{\n\tmargin: 1em auto 0;\n\tmax-width: 1000px;\n\tbackground-color: #ddd;\n\toverflow-x: hidden;\n}\n.main{\n\tpadding: .5em 0;\n\tmargin: 0 .5em;\n\tdisplay: flex;\n\tgap: 10px;\n\tbackground-color: white;\n\tborder-bottom: thin solid black;\n\tborder-image: linear-gradient(to right, white, silver 10%, silver 90%, white);\n\tborder-image-slice: 1;\n}\n.main>p{\n\tmargin: auto 0;\n\theight: 100%;\n}\n.main>p:first-child{\n\tmin-width: 9em;\n\tmax-width: 9em;\n\tpadding-right: .5em;\n\ttext-align: end;\n}\n.main::before{\n\tcontent: \"\";\n\tposition: absolute;\n\tleft: 9em;\n\ttop: 4px;\n\twidth: 2.5px;\n\theight: calc(100% - 8px);\n\tbackground-color: var(--c);\n}\n.chat{\n\tmargin-left: 4em;\n\tmargin-right: 0;\n\tbackground-color: #f8f8f8;\n\tfont-size: .8em;\n}\n.chat>p{\n\tcolor: var(--c);\n\tfilter: brightness(70%);\n}\n.secret{\n\tbackground-color: #666;\n}\n.secret>p{\n\tfont-weight: bold;\n\tfilter: brightness(200%);\n}\n.secret>p>span{\n\tfont-size: .5em;\n}\n.secret::after{\n\tcontent: \"\";\n\tposition: absolute;\n\ttop: 0;\n\tleft: 0;\n\tdisplay: block;\n\theight: 100%;\n\twidth: 5px;\n\tbackground-color: red;\n}\n.info{\n\tmargin: 5px;\n\tletter-spacing: .1em;\n\tfont-weight: bold;\n\tfont-family: system-ui;\n}\n.info>span{\n\tfont-size: .8em;\n}\n.info>p{\n\ttext-align: center;\n}","header":"<p>{filename}</p>","footer":"","option":{"option_rubyCut":true},"comment":[[1,"system","\t<div class=\"info\" style=\"--c:{color}\">\n\t\t<p>{text}</p>\n\t</div>"],[0,"メイン","\t<div class=\"main\" style=\"--c:{color}\">\n\t\t<p>{author}</p>\n\t\t<p>{text}</p>\n\t</div>"],[0,"雑談","\t<div class=\"main chat\" style=\"--c:{color}\">\n\t\t<p>{author}</p>\n\t\t<p>{text}</p>\n\t</div>"],[0,"情報","\t<div class=\"info\" style=\"--c:{color}\">\n\t\t<span>{author}</span>\n\t\t<p>{text}</p>\n\t</div>"],[2,"秘匿\\((.+?),(.+?)\\.\\.\\)","\t<div class=\"main chat secret\" style=\"--c:{color}\">\n\t\t<p>{author}<br><span>$1 - $2</span></p>\n\t\t<p>{text}</p>\n\t</div>"],[2,"(その他)?.*","\t<div class=\"info\" style=\"--c:{color}\">\n\t\t<span>{author}</span>\n\t\t<p>{text}</p>\n\t</div>"]]}};
 	const req=result.html;
-	let css=req.shift(),
-		header=req.shift(),
-		footer=req.shift();
-	css=css[2];
-	header=decodeURI(header[2].replace(/\{filename\}/g,location.href.match(/(?<=\/)[^\/\.]+(?=\.[^\/\.]+)/)));
-	footer=decodeURI(footer[2].replace(/\{filename\}/g,location.href.match(/(?<=\/)[^\/\.]+(?=\.[^\/\.]+)/)));
+	let css=req.css,
+		header=req.header,
+		footer=req.footer,
+		comment=req.comment,
+		option=req.option;
+	header=decodeURI(header.replace(/\{filename\}/g,location.href.match(/(?<=\/)[^\/\.]+(?=\.[^\/\.]+)/)));
+	footer=decodeURI(footer.replace(/\{filename\}/g,location.href.match(/(?<=\/)[^\/\.]+(?=\.[^\/\.]+)/)));
 	document.head.innerHTML=document.head.innerHTML.replace(/(?<=<title>).+(?=<\/title>)/,decodeURI(location.href.match(/(?<=\/)[^\/\.]+(?=\.[^\/\.]+)/)))+"<style>"+css+"</style>";
 	document.body.innerHTML="<header>"+header+"</header>" + document.body.innerHTML + "<footer>"+footer+"</footer>";
 	let p=document.querySelector("body>p");
 	while(p.tagName=="P"){
-		const tab=p.children[0].textContent.replace(/\s\[|\]/g,""),
+		let tab=p.children[0].textContent.replace(/\s\[|\]/g,""),
 			author=p.children[1].textContent.replace(/[@＠・]/g,"$&<wbr>"),
 			text=p.children[2].innerHTML,
 			color=p.style.color;
+		if(option.option_rubyCut)author=author.replace(/[\(（][a-zA-Zぁ-んァ-ンｦ-ﾟ・ー\s]+[）\)]/,"")
 		var div=document.createElement("div");
 		div.classList.add("container");
-		let check=false;
-		for(var j=0;j<req.length;j++){
-			switch(req[j][0]){
+		for(var j=0;j<comment.length;j++){
+			var cond=false;
+			var rt="", reg="";
+			switch(comment[j][0]){
 				case 0:
-					if(tab==req[j][1]){
-						div.innerHTML=req[j][2].replace(/\{tab\}/g,tab)
-							.replace(/\{author\}/g,author)
-							.replace(/\{text\}/g,text)
-							.replace(/\{color\}/g,color);
-						check=true;
-					}
+					cond=(tab==comment[j][1]);
 					break;
 				case 1:
-					if(author==req[j][1]){
-						div.innerHTML=req[j][2].replace(/\{tab\}/g,tab)
-							.replace(/\{author\}/g,author)
-							.replace(/\{text\}/g,text)
-							.replace(/\{color\}/g,color);
-						check=true;
-					}
+					cond=(author==comment[j][1]);
+					break;
+				case 2:
+					cond=(new RegExp(comment[j][1]).test(tab));
+					rt=tab;
+					reg=new RegExp(comment[j][1]);
+					break;
+				case 3:
+					cond=(new RegExp(comment[j][1]).test(author));
+					rt=author;
+					reg=new RegExp(comment[j][1]);
 					break;
 			}
-			if(check)break;
+			if(cond){
+				var resultText = comment[j][2].replace(/\{tab\}/g,tab)
+					.replace(/\{author\}/g,author)
+					.replace(/\{text\}/g,text)
+					.replace(/\{color\}/g,color);
+				if(reg!="") resultText = rt.match(reg)[0].replace(reg,resultText);
+				div.innerHTML=resultText;
+				break;
+			}
 		}
-		if(!check) div.innerHTML=p.innerHTML;
+		if(!div.innerHTML) div.innerHTML=p.innerHTML;
 		if(p.nextElementSibling!=null){
 			var temp=p;
 			p=p.nextElementSibling;
--- option.js
+++ option.js
@@ -1,6 +1,9 @@
+var def={"html":{"css":"div,p,span{\n\tpadding: 0;\n\tmargin: 0;\n\tposition: relative;\n\tbox-sizing: border-box;\n}\nheader{\n\tposition: fixed;\n\ttop: 0;\n\tleft: 0;\n\theight: 1rem;\n\twidth: 100%;\n\tbackground: black;\n\tcolor: white;\n\tfont-size: .8em;\n\tz-index: 1;\n}\nbody{\n\tmargin: 1em auto 0;\n\tmax-width: 1000px;\n\tbackground-color: #ddd;\n\toverflow-x: hidden;\n}\n.main{\n\tpadding: .5em 0;\n\tmargin: 0 .5em;\n\tdisplay: flex;\n\tgap: 10px;\n\tbackground-color: white;\n\tborder-bottom: thin solid black;\n\tborder-image: linear-gradient(to right, white, silver 10%, silver 90%, white);\n\tborder-image-slice: 1;\n}\n.main>p{\n\tmargin: auto 0;\n\theight: 100%;\n}\n.main>p:first-child{\n\tmin-width: 9em;\n\tmax-width: 9em;\n\tpadding-right: .5em;\n\ttext-align: end;\n}\n.main::before{\n\tcontent: \"\";\n\tposition: absolute;\n\tleft: 9em;\n\ttop: 4px;\n\twidth: 2.5px;\n\theight: calc(100% - 8px);\n\tbackground-color: var(--c);\n}\n.chat{\n\tmargin-left: 4em;\n\tmargin-right: 0;\n\tbackground-color: #f8f8f8;\n\tfont-size: .8em;\n}\n.chat>p{\n\tcolor: var(--c);\n\tfilter: brightness(70%);\n}\n.secret{\n\tbackground-color: #666;\n}\n.secret>p{\n\tfont-weight: bold;\n\tfilter: brightness(200%);\n}\n.secret>p>span{\n\tfont-size: .5em;\n}\n.secret::after{\n\tcontent: \"\";\n\tposition: absolute;\n\ttop: 0;\n\tleft: 0;\n\tdisplay: block;\n\theight: 100%;\n\twidth: 5px;\n\tbackground-color: red;\n}\n.info{\n\tmargin: 5px;\n\tletter-spacing: .1em;\n\tfont-weight: bold;\n\tfont-family: system-ui;\n}\n.info>span{\n\tfont-size: .8em;\n}\n.info>p{\n\ttext-align: center;\n}","header":"<p>{filename}</p>","footer":"","option":{"option_rubyCut":true},"comment":[[1,"system","\t<div class=\"info\" style=\"--c:{color}\">\n\t\t<p>{text}</p>\n\t</div>"],[0,"メイン","\t<div class=\"main\" style=\"--c:{color}\">\n\t\t<p>{author}</p>\n\t\t<p>{text}</p>\n\t</div>"],[0,"雑談","\t<div class=\"main chat\" style=\"--c:{color}\">\n\t\t<p>{author}</p>\n\t\t<p>{text}</p>\n\t</div>"],[0,"情報","\t<div class=\"info\" style=\"--c:{color}\">\n\t\t<span>{author}</span>\n\t\t<p>{text}</p>\n\t</div>"],[2,"秘匿\\((.+?),(.+?)\\.\\.\\)","\t<div class=\"main chat secret\" style=\"--c:{color}\">\n\t\t<p>{author}<br><span>$1 - $2</span></p>\n\t\t<p>{text}</p>\n\t</div>"],[2,"(その他)?.*","\t<div class=\"info\" style=\"--c:{color}\">\n\t\t<span>{author}</span>\n\t\t<p>{text}</p>\n\t</div>"]]}};
 chrome.storage.local.get("html", function(result){
-	if(result.html==undefined)result={"html":[[0,"css","div,p,span{\n\tpadding:0;margin:0;\n\tposition:relative;\n\tbox-sizing:border-box;\n}\nheader{\n\tposition:fixed;\n\ttop:0; left:0;\n\theight:1rem;\n\twidth:100%;\n\tbackground:black;\n\tcolor:white;\n\tfont-size:0.8em;\n\tz-index:1;\n}\nbody{\n\tmargin:1em auto 0;\n\tmax-width:1000px;\n\tword-break:keep-all;\n\tbackground-color:#eee;\n\toverflow-x:hidden;\n}\n.main,\n.chat{\n\tpadding:0.5em 0;\n\tdisplay:flex;\n\tgap:10px;\n\tborder-bottom:thin solid black;\n\tborder-image:linear-gradient(to right,white,silver 10%,silver 90%,white);\n\tborder-image-slice:1;\n}\n.main>p,\n.chat>p{\n\tmargin:auto 0;\n\theight:100%;\n}\n.chat>p{\n\tcolor:var(--c);\n}\n.main>p:first-child,\n.chat>p:first-child{\n\tmin-width:9em;\n\tmax-width:9em;\n\tpadding-left:0.5em;\n}\n.main::before,\n.chat::before{\n\tcontent:\"\";\n\tposition:absolute;\n\tleft:9em; top:4px;\n\twidth:2px; height:calc(100% - 8px);\n\tbackground-color:var(--c);\n}\n\n.main{\n\tbackground-color:white;\n}\n.chat{\n\tmargin-left:4em;\n\tbackground-color:#f8f8f8;\n}\n.info{\n\tmargin:5px;\n\tletter-spacing:0.1em;\n\tfont-weight:bold;\n\tfont-family:system-ui;\n}\n.info>span{\n\tfont-size:.8em;\n}\n.info>p{\n\ttext-align:center;\n}"],[0,"header","<p>{filename}</p>"],[0,"footer",""],[1,"system","\t<div class=\"info\" style=\"--c:{color}\">\n\t\t<p>{text}</p>\n\t</div>"],[0,"メイン","\t<div class=\"main\" style=\"--c:{color}\">\n\t\t<p>{author}</p>\n\t\t<p>{text}</p>\n\t</div>"],[0,"雑談","\t<div class=\"chat\" style=\"--c:{color}\">\n\t\t<p>{author}</p>\n\t\t<p>{text}</p>\n\t</div>"],[0,"情報","\t<div class=\"info\" style=\"--c:{color}\">\n\t\t<span>{author}</span>\n\t\t<p>{text}</p>\n\t</div>"]]};
-	document.querySelector(".css textarea").value = result.html[0][2];
+	if(result.html==undefined)result=def;
+	console.log("デバッグ用だから見ちゃやーや。");
+	console.log(result);
+	document.querySelector(".css textarea").value = result.html.css;
 	document.querySelector(".css textarea").addEventListener("input",(e)=>{
 		var viewCss=e.currentTarget.value;
 		for(elem of document.querySelectorAll(".req iframe")){
@@ -9,16 +12,15 @@ chrome.storage.local.get("html", function(result){
 		document.getElementById("viewCss").innerHTML=viewCss;
 	})
 	document.querySelector(".css textarea").addEventListener("keydown",(e)=>{tabkey(e)})
-	document.querySelector(".header textarea").value = result.html[1][2];
-	document.querySelector(".footer textarea").value = result.html[2][2];
-	for(var i=3;i<result.html.length;i++){
+	document.querySelector(".header textarea").value = result.html.header;
+	document.querySelector(".footer textarea").value = result.html.footer;
+	for(var i=0;i<result.html.comment.length;i++){
 		var req=document.createElement("div");
 		req.classList.add("req","flex");
-		if(result.html[i][0]){
-			req.innerHTML='<div class="vflex"><div class="head flex"><input type="text" value="'+result.html[i][1]+'"><label><input type="checkbox" checked><div class="check"><p>タブ</p><p>発言者</p></div></label><button class="up"></button><button class="down"></button><button class="delete"></button></div><div class="textarea"><textarea>'+result.html[i][2]+'</textarea></div></div><iframe>';
-		}else{
-			req.innerHTML='<div class="vflex"><div class="head flex"><input type="text" value="'+result.html[i][1]+'"><label><input type="checkbox"><div class="check"><p>タブ</p><p>発言者</p></div></label><button class="up"></button><button class="down"></button><button class="delete"></button></div><div class="textarea"><textarea>'+result.html[i][2]+'</textarea></div></div><iframe>';
-		}
+		var a="", b="";
+		if(result.html.comment[i][0]%2==1) a=" checked";
+		if(result.html.comment[i][0]>=2) b=" checked";
+		req.innerHTML='<div class="vflex"><div class="head flex"><input type="text" value="'+result.html.comment[i][1]+'"><label class="regex"><input type="checkbox"'+b+'><p>正規表現</p></label><label class="cond"><input type="checkbox"'+a+'><div class="check"><p>タブ</p><p>発言者</p></div></label><button class="up"></button><button class="down"></button><button class="delete"></button></div><div class="textarea"><textarea>'+result.html.comment[i][2]+'</textarea></div></div><iframe>';
 		req.querySelector("button.up").addEventListener("click",(e)=>{moveUp(e)});
 		req.querySelector("button.down").addEventListener("click",(e)=>{moveDown(e)});
 		req.querySelector("button.delete").addEventListener("click",(e)=>{reqDelete(e)});
@@ -29,13 +31,16 @@ chrome.storage.local.get("html", function(result){
 	for(elem of document.querySelectorAll(".req")){
 		var viewCss=document.createElement("style");
 		viewCss.id="viewCss";
-		viewCss.innerHTML=result.html[0][2];
+		viewCss.innerHTML=result.html.css;
 		elem.querySelector("iframe").contentWindow.document.head.appendChild(viewCss);
 		var viewHtml=elem.querySelector("textarea").value.replace(/\{tab\}/g,"タブ").replace(/\{author\}/g,"発言者").replace(/\{text\}/g,"これはデモテキストです。\n発言はこのように表示されます。").replace(/\{color\}/g,"#00ffff");
 		var container=document.createElement("p");
 		container.classList.add("container");
 		container.innerHTML=viewHtml;
 		elem.querySelector("iframe").contentWindow.document.body.appendChild(container);
+	}
+	for(key in result.html.option){
+		document.querySelector("input#"+key).checked=result.html.option[key];
 	}
 });
 document.querySelector("#fix>input").addEventListener("change",function(){
@@ -51,7 +56,7 @@ document.getElementById("save").addEventListener("click",function(){
 document.getElementById("add").addEventListener("click",function(){
 	var req=document.createElement("div");
 	req.classList.add("req","flex");
-	req.innerHTML='<div class="vflex"><div class="head flex"><input type="text" value="new"><label><input type="checkbox"><div class="check"><p>タブ</p><p>発言者</p></div></label><button class="up"></button><button class="down"></button><button class="delete"></button></div><div class="textarea"><textarea></textarea></div></div><iframe>';
+	req.innerHTML='<div class="vflex"><div class="head flex"><input type="text" value="new"><label class="regex"><input type="checkbox"><p>正規表現</p></label><label class="cond"><input type="checkbox"><div class="check"><p>タブ</p><p>発言者</p></div></label><button class="up"></button><button class="down"></button><button class="delete"></button></div><div class="textarea"><textarea></textarea></div></div><iframe>';
 	req.querySelector("button.up").addEventListener("click",(e)=>{moveUp(e)});
 	req.querySelector("button.down").addEventListener("click",(e)=>{moveDown(e)});
 	req.querySelector("button.delete").addEventListener("click",(e)=>{reqDelete(e)});
@@ -69,6 +74,54 @@ document.getElementById("add").addEventListener("click",function(){
 	container.innerHTML=viewHtml;
 	elem.querySelector("iframe").contentWindow.document.body.appendChild(container);
 });
+document.getElementById("reset").addEventListener("click",function(){
+	var flag=confirm("設定をデフォルトに戻します。（保存をしない限りはページ再読み込みで元に戻せます）\nリセットしてもよろしいですか？");
+	if(flag==true){
+		for(var elem of document.querySelectorAll(".req")) elem.remove();
+		result=def;
+		console.log("デバッグ用だから見ちゃやーや。");
+		console.log(result);
+		document.querySelector(".css textarea").value = result.html.css;
+		document.querySelector(".css textarea").addEventListener("input",(e)=>{
+			var viewCss=e.currentTarget.value;
+			for(elem of document.querySelectorAll(".req iframe")){
+				elem.contentWindow.document.getElementById("viewCss").innerHTML=viewCss;
+			}
+			document.getElementById("viewCss").innerHTML=viewCss;
+		})
+		document.querySelector(".css textarea").addEventListener("keydown",(e)=>{tabkey(e)})
+		document.querySelector(".header textarea").value = result.html.header;
+		document.querySelector(".footer textarea").value = result.html.footer;
+		for(var i=0;i<result.html.comment.length;i++){
+			var req=document.createElement("div");
+			req.classList.add("req","flex");
+			var a="", b="";
+			if(result.html.comment[i][0]%2==1) a=" checked";
+			if(result.html.comment[i][0]>=2) b=" checked";
+			req.innerHTML='<div class="vflex"><div class="head flex"><input type="text" value="'+result.html.comment[i][1]+'"><label class="regex"><input type="checkbox"'+b+'><p>正規表現</p></label><label class="cond"><input type="checkbox"'+a+'><div class="check"><p>タブ</p><p>発言者</p></div></label><button class="up"></button><button class="down"></button><button class="delete"></button></div><div class="textarea"><textarea>'+result.html.comment[i][2]+'</textarea></div></div><iframe>';
+			req.querySelector("button.up").addEventListener("click",(e)=>{moveUp(e)});
+			req.querySelector("button.down").addEventListener("click",(e)=>{moveDown(e)});
+			req.querySelector("button.delete").addEventListener("click",(e)=>{reqDelete(e)});
+			req.querySelector("textarea").addEventListener("input",(e)=>{htmlView(e)});
+			req.querySelector("textarea").addEventListener("keydown",(e)=>{tabkey(e)})
+			document.getElementById("html").insertBefore(req,document.getElementById("save"));
+		}
+		for(elem of document.querySelectorAll(".req")){
+			var viewCss=document.createElement("style");
+			viewCss.id="viewCss";
+			viewCss.innerHTML=result.html.css;
+			elem.querySelector("iframe").contentWindow.document.head.appendChild(viewCss);
+			var viewHtml=elem.querySelector("textarea").value.replace(/\{tab\}/g,"タブ").replace(/\{author\}/g,"発言者").replace(/\{text\}/g,"これはデモテキストです。\n発言はこのように表示されます。").replace(/\{color\}/g,"#00ffff");
+			var container=document.createElement("p");
+			container.classList.add("container");
+			container.innerHTML=viewHtml;
+			elem.querySelector("iframe").contentWindow.document.body.appendChild(container);
+		}
+		for(key in result.html.option){
+			document.querySelector("input#"+key).checked=result.html.option[key];
+		}
+	}
+})
 function reqDelete(e){
 	var elem=e.currentTarget.parentNode.parentNode.parentNode;
 	elem.parentNode.removeChild(elem);
@@ -130,16 +183,24 @@ function tabkey(e){
 	}
 }
 function saveData(){
-		var array=[];
-		array.push([0,"css",document.querySelector(".css textarea").value]);
-		array.push([0,"header",document.querySelector(".header textarea").value]);
-		array.push([0,"footer",document.querySelector(".footer textarea").value]);
+		var array={};
+		array.css=document.querySelector(".css textarea").value;
+		array.header=document.querySelector(".header textarea").value;
+		array.footer=document.querySelector(".footer textarea").value;
+		array.option={};
+		for(elem of document.querySelectorAll("#option input[type='checkbox']")){
+			array.option[elem.id]=elem.checked;
+		}
+		array.comment=[];
 		for(elem of document.querySelectorAll("div.req")){
+			var type1=elem.querySelector("label.cond input[type='checkbox']").checked ? 1 : 0;
+			var type2=elem.querySelector("label.regex input[type='checkbox']").checked ? 1 : 0;
+			var type=type1+type2*2;
 			var name=elem.querySelector("input[type='text']").value;
 			var text=elem.querySelector("textarea").value;
-			var type=elem.querySelector("input[type='checkbox']").checked ? 1 : 0;
-			array.push([type,name,text]);
+			array.comment.push([type,name,text]);
 		}
+		console.log({html:array});
 		chrome.storage.local.set({html : array}, function(){});
 }
 window.addEventListener("keydown",function(e){
